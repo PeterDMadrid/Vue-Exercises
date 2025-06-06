bd7bf191bfc166b7b966a60eeb1df05293d330d6
Peter Madrid <petermadrid0421@gmail.com>
2025-06-06 13:40:21 +0800

    text-interolation

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+const mymessage = "Yellow World"
+const myNumber = 100
+function add(x, y) {
+    return x + y
+}
+</script>
+
+<template>
+    <h1>{{ mymessage }}</h1>
+    <h2>{{ myNumber }}</h2>
+    <h2>2 + 2 = {{ 2 + 2 }}</h2>
+    <h1>Add Two Numbers: {{ add(2, 5) }}</h1>
+</template>
