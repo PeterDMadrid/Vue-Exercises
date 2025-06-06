5794293787172556cd1b4c162677b8e4a9b42124
Peter Madrid <petermadrid0421@gmail.com>
2025-06-06 14:17:23 +0800

    styling

---
+++ src/components/LocalStyle.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2 class="g-1">This is Local</h2>
+</template>
+
+<style></style>
---
+++ src/components/ModuleStyle.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- <h1 :class="$style['m-class']">Using Module Css with $ syntax kebab case</h1> -->
+    <h1 :class="myClass.anotherClass">Using Module CSS using $ camel case</h1>
+</template>
+
+<style module="myClass">
+.m-class {
+    background: firebrick;
+    color: white;
+}
+
+.anotherClass {
+    background: seagreen;
+    color: white;
+}
+</style>
